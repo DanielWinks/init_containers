400a53d6f59aaa313bb715d5d57561737b112e46
Daniel Winks <daniel.winks@gmail.com>
2021-03-12 14:54:43 -0500

    Update env var

--- mariadb/createdb.sh
+++ mariadb/createdb.sh
@@ -1,21 +1,21 @@
 #!/bin/ash
 
-mysql -h $HOSTNAME -uroot -p$MYSQL_ROOT_PASSWORD -e \
+mysql -h $HOSTNAME -uroot -p$MARIADB_ROOT_PASSWORD -e \
     "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;"
 
-mysql -h $HOSTNAME -uroot -p$MYSQL_ROOT_PASSWORD -e \
+mysql -h $HOSTNAME -uroot -p$MARIADB_ROOT_PASSWORD -e \
     "CREATE USER IF NOT EXISTS '$MARIADB_USER' IDENTIFIED \
     BY '$MARIADB_USER_PASSWORD';"
 
-mysql -h $HOSTNAME -uroot -p$MYSQL_ROOT_PASSWORD -e \
+mysql -h $HOSTNAME -uroot -p$MARIADB_ROOT_PASSWORD -e \
     "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%' \
     IDENTIFIED BY '$MARIADB_USER_PASSWORD';"
 
-mysql -h $HOSTNAME -uroot -p$MYSQL_ROOT_PASSWORD -e \
+mysql -h $HOSTNAME -uroot -p$MARIADB_ROOT_PASSWORD -e \
     "FLUSH PRIVILEGES;"
 
-mysql -h $HOSTNAME -uroot -p$MYSQL_ROOT_PASSWORD -e \
+mysql -h $HOSTNAME -uroot -p$MARIADB_ROOT_PASSWORD -e \
     "show databases;"
 
-mysql -h $HOSTNAME -uroot -p$MYSQL_ROOT_PASSWORD -e \
+mysql -h $HOSTNAME -uroot -p$MARIADB_ROOT_PASSWORD -e \
     "select host, user from mysql.user;"
