9e421d7f06ce0f912da833b548d6126606ce23b4
Daniel Winks <daniel.winks@gmail.com>
2021-03-12 14:34:42 -0500

    Update mariadb check

--- home_assistant/rootfs/check_mariadb.sh
+++ home_assistant/rootfs/check_mariadb.sh
@@ -2,8 +2,8 @@
 
 check_mariadb() {
     mysql --protocol=tcp -h mariadb.db \
-    -u$MYSQL_USER -p$MYSQL_ROOT_PASSWORD -e "show databases;"\
-    | grep home_assistant >/dev/null
+    -uroot -p$MARIADB_ROOT_PASSWORD -e "show databases;"\
+    | grep $HOME_ASSISTANT_DB >/dev/null
 }
 check_mariadb
 connected=$?
@@ -13,4 +13,4 @@ until [ "$connected" -eq 0 ]; do
     echo "Waiting for connection to mariadb..."
     sleep 5s
 done
-echo "Successfully connected to home_assistant mariadb database."
+echo "Successfully connected to $HOME_ASSISTANT_DB mariadb database."
